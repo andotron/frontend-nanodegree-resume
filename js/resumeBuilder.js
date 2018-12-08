8910e907ce3c230071371ff410f305a5c46c8eea
Andy Chen <andy@Andys-MacBook-Pro.local>
2018-12-07 20:03:39 -0800

    reluctantly conformed to the format of current repo for the sakes of everything working correctly

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,58 +1,100 @@
 var bio = {
     name:'Andy',
     role:'Aspiring Front-End Engineer',
-    contactInfo: {
+    contacts: {
         mobile:'555-555-5555',
         email: 'achen210@gmail.com', 
         twitter: '@iandosan',
         github: 'andotron',
         location: 'California'
     },
-    picture: 'images/me.jpg',
+    biopic: 'images/me.jpg',
     welcomeMessage: 'Hello, welcome to my online resume',
     skills: ['JavaScript', 'CSS', 'HTML']
 };
 
 var work = {
-    currentJob: 'Student',
-    employer: 'Self',
-    yearsWorked: 8,
-    city: 'SF'
+    jobs: [
+        {
+            employer: 'Self',
+            title: 'Student',
+            yearsWorked: 8,
+            city: 'SF'
+        }
+    ]
 }
 
 var education = {
-    school: 'UCSC',
-    yearsAttended: "2012 - 2014",
-    city: 'Santa Cruz'
+    schools: [
+        {    
+            name: 'UCSC',
+            dates: '2012 - 2014',
+            location: 'Santa Cruz',
+            degree: 'B.S Biological Sciences',
+            major: ['Biology']
+        }
+
+    ]
 }
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-//contact
-var formattedMobileInfo = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
-var formattedEmailInfo = HTMLemail.replace("%data%", bio.contactInfo.email);
-var formattedTwitterInfo = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
-var formattedGitHub = HTMLgithub.replace("%data%", bio.contactInfo.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
-var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
-var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-var formattedSkill = HTMLskills.replace("%data%", bio.skills);
-//work
-var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.currentJob);
-var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.employer);
-var formattedWorkDates = HTMLworkDates.replace("%data%", work.yearsWorked);
-var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.city);
+var projects = {
+    projects:[
+        {   title: 'frontEnd nano degree',
+            dates: '2018',
+            description: 'Recreate an online resume'
+        }
+    ]
+}
 
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// //contact
+// var formattedMobileInfo = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
+// var formattedEmailInfo = HTMLemail.replace("%data%", bio.contactInfo.email);
+// var formattedTwitterInfo = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
+// var formattedGitHub = HTMLgithub.replace("%data%", bio.contactInfo.github);
+// var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
+// var formattedPicture = HTMLbioPic.replace("%data%", bio.picture);
+// var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+// var formattedSkill = HTMLskills.replace("%data%", bio.skills);
+// //work
+// var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.currentJob);
+// var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+// var formattedWorkDates = HTMLworkDates.replace("%data%", work.yearsWorked);
+// var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.city);
+// //school
+// var formattedSchoolName = HTMLschoolName.replace("%data%", education.school);
+// var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.degree);
+// var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.yearsAttended)
+// var formattedSchoolLocation = HTMLlocation.replace("%data%", education.city);
+// var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.major);
+// // var formatted.replace("%data%")
+// // var formatted.replace("%data%")
+// // var formatted.replace("%data%")
+// // var formatted.replace("%data%")
+// // var formatted.replace("%data%")
+// // var formatted.replace("%data%")
+// // var formatted.replace("%data%")
+// // var formatted.replace("%data%")
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+// $("#topContacts").append(formattedMobileInfo);
+// $("#topContacts").append(formattedEmailInfo);
+// $("#topContacts").append(formattedTwitterInfo);
+// $("#topContacts").append(formattedGitHub);
+// $("#topContacts").append(formattedLocation);
+// $("#header").append(formattedPicture);
+// $("#header").append(formattedWelcomeMsg);
+// $("#header").append(formattedSkill);
 
-$("#topContacts").append(formattedMobileInfo);
-$("#topContacts").append(formattedEmailInfo);
-$("#topContacts").append(formattedTwitterInfo);
-$("#topContacts").append(formattedGitHub);
-$("#topContacts").append(formattedLocation);
-$("#header").append(formattedPicture);
-$("#header").append(formattedWelcomeMsg);
-$("#header").append(formattedSkill);
+// $("#workExperience").append(formattedWorkTitle);
+// $("#workExperience").append(formattedWorkEmployer);
+// $("#workExperience").append(formattedWorkDates);
+// $("#workExperience").append(formattedWorkLocation);
 
+// $("#education").append(formattedSchoolName);
+// $("#education").append(formattedSchoolDates);
+// $("#education").append(formattedSchoolLocation);
+// $("#education").append(formattedSchoolDegree);
+// $("#education").append(formattedSchoolLocation);
